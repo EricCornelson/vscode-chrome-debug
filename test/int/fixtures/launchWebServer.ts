1df51414d6241c85648a55e8f40f0a0cf8f99d90
Eric Cornelson <Eric.Cornelson@microsoft.com>
2019-07-08 12:08:08 -0700

    Fixing tests to work in V1

--- test/int/fixtures/launchWebServer.ts
+++ test/int/fixtures/launchWebServer.ts
@@ -75,7 +75,7 @@ export class ProvideStaticUrl implements IFixture {
 
     public constructor(public readonly url: URL, private readonly testSpec: TestProjectSpec) {}
 
-    public get launchConfig(): ILaunchRequestArgs {
+    public get launchConfig(): any {
         return {...this.testSpec.props.launchConfig, url: this.url.href };
     }
     cleanUp() {}
